aa1518f38a6849896667e321a7daafa10feeeafc
DeZmEnD11 <56726482+DeZmEnD11@users.noreply.github.com>
2020-12-27 18:00:57 +0200

    3la allah

---
+++ ImageController.php
@@ -0,0 +1,83 @@
+<html>
+
+  <head>
+  <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/fontAwesome.css">
+        <link rel="stylesheet" href="css/hero-slider.css">
+        <link rel="stylesheet" href="css/owl-carousel.css">
+        <link rel="stylesheet" href="css/datepicker.css">
+        <link rel="stylesheet" href="css/templatemo-style.css">
+
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    <style>
+        
+    
+        body{
+          background-color: #F2F2F2;
+        }
+          </style>
+    
+    
+        <!-- Theme Style -->
+        <link rel="stylesheet" href="css/style.css">
+      </head>
+
+
+
+<body>
+<?php
+include "menu.php";
+?>
+<section class="container">
+<div class="row">
+  <div class="column">
+    <div class="col-md-12" style="width: 20rem;">
+  <img src="img/addimage.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Add New B-scan Image</h5>
+    <p class="card-text">You can add new Images </p>
+    <a href="AddDoctorView.php" class="btn btn-primary">Add</a>
+  </div>
+</div>
+  </div>
+    
+  <div class="column">
+    <div class="col-md-12" style="width: 20rem;">
+  <img src="img/dell.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Delete B-Scan Image</h5>
+    <p class="card-text">Delete Selected Image </p>
+    <a href="DeleteDoctorView.php" class="btn btn-primary">Delete</a>
+  </div>
+</div>
+  </div>
+    
+  <div class="column">
+    <div class="col-md-12" style="width: 20rem;">
+  <img src="img/update.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Update B-Scan Image</h5>
+    <p class="card-text">Update Selected Image info </p>
+    <a href="EditDoctorView.php" class="btn btn-primary">Update</a>
+  </div>
+</div>
+  </div>
+  <div class="column">
+  
+  </div>
+</div>
+   </section> 
+   <?php
+include "footer.php";
+?>
+
+</body>
+    
+
+
+</html>
--- menu.php
+++ menu.php
@@ -6,8 +6,7 @@
         <header id="header">
             <div class="container">
                 <div class="row">
-
-                    <div class="col-md-12"">
+                    <div class="col-md-12">
                         <button id="primary-nav-button" type="button">Menu</button>
                         <a href="index.html"><div class="logo">
                             <img src="img/logo.png" alt="Venue Logo">
@@ -20,7 +19,7 @@
                                 <li class='active'><a href="news.php">News</a></li>
                                 <li class='active'><a href="services.php">Our Services</a></li>
                                 <li class='active'><a href="contactus.php">Contact Us</a></li>
-
+                                   
                             </ul>
                         </nav>
                     </div>
